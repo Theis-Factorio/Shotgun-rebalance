99d4c0d6299622bb8dab872ff857ed611647bea1
theis999 <theis99999@gmail.com>
2024-11-24 13:06:05 +0100

    Update to 2.0

--- data.lua
+++ data.lua
@@ -3,57 +3,38 @@ data.raw.recipe["shotgun"].enabled = true
 data.raw.recipe["shotgun-shell"].enabled = true
 
 --Decrease magazine size of shotgun shells 
-data.raw.ammo["shotgun-shell"].magazine_size = 8
+data.raw.ammo["shotgun-shell"].magazine_size = 6
 
-data.raw.character.character.running_speed = -0.01
 --Decrease movement_slow_down_factor for shotguns
-data.raw.gun["pistol"].attack_parameters.movement_slow_down_factor = -50 --was 0.6
-data.raw.gun["shotgun"].attack_parameters.movement_slow_down_factor = 0.5 --was 0.6
-data.raw.gun["combat-shotgun"].attack_parameters.movement_slow_down_factor = 0.3 --was 0.5
+--data.raw.gun["pistol"].attack_parameters.movement_slow_down_factor = 0.1 --was 0.2
+--data.raw.gun["shotgun"].attack_parameters.movement_slow_down_factor = 0.4 --was 0.6
+data.raw.gun["shotgun"].attack_parameters.movement_slow_down_cooldown = 30 -- was default 60 ticks 
+data.raw.gun["combat-shotgun"].attack_parameters.movement_slow_down_factor = 0.4 --was 0.5
 
---Remove shotgun from unlocked by military 1
+data.raw.projectile["piercing-shotgun-pellet"].action.action_delivery.target_effects.damage.amount = 10 -- was 8, however normal shotgun pellets are 8
+data.raw.projectile["piercing-shotgun-pellet"].piercing_damage = 50 -- new
+data.raw.ammo["piercing-shotgun-shell"].ammo_type.action[2].action_delivery.max_range = 17 -- Was 15, the same as normal shotgun pellets
+
+--Remove shotgun unlocked from by military 1
 data.raw.technology["military"].effects =
 {
-    {
-        type = "unlock-recipe",
-        recipe = "submachine-gun"
-    }
+    { type = "unlock-recipe", recipe = "submachine-gun" }
 }
 
 --Move piercing-shotgun-shell to military 2
 data.raw.technology["military-2"].effects =
 {
-    {
-        type = "unlock-recipe",
-        recipe = "piercing-rounds-magazine"
-    },
-    {
-        type = "unlock-recipe",
-        recipe = "piercing-shotgun-shell"
-    },
-    {
-        type = "unlock-recipe",
-        recipe = "grenade"
-    }
+    { type = "unlock-recipe", recipe = "piercing-rounds-magazine" },
+    { type = "unlock-recipe", recipe = "grenade" },
+    { type = "unlock-recipe", recipe = "piercing-shotgun-shell" }, -- new
 }
 
 --Ignore military 3
 
---Remove piercing-shotgun-shell to military 4 and add small damage and firing speed to military 3
+--Remove piercing-shotgun-shell from military 4 and add small damage and firing speed boost
 data.raw.technology["military-4"].effects =
 {
-    {
-        type = "unlock-recipe",
-        recipe = "cluster-grenade"
-    },
-    {
-        type = "gun-speed",
-        ammo_category = "shotgun-shell",
-        modifier = 0.1
-    },
-    {
-        type = "ammo-damage",
-        ammo_category = "shotgun-shell",
-        modifier = 0.1
-    }
+    { type = "unlock-recipe", recipe = "cluster-grenade" },
+    { type = "gun-speed", ammo_category = "shotgun-shell", modifier = 0.2 }, -- new
+    { type = "ammo-damage", ammo_category = "shotgun-shell", modifier = 0.2 }, -- new
 }
